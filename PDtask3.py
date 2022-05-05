4f9b2c0a7ca116cbd27583852f3d249be1b55832
dsamoob <dsamoob@yandex.ru>
2022-05-05 09:57:49 +0300

    Create PDtask3.py

---
+++ PDtask3.py
@@ -0,0 +1,50 @@
+def acquaintance(first_list, second_list):
+    if len(first_list) != len(second_list):
+        print(f'Списки разного размера - не получится спарить')
+    else:
+        coupling = list(zip(sorted(first_list), sorted(second_list)))
+        for i in coupling:
+            print(f'{i[0]} и {i[1]}')
+        print(f'\n')
+
+
+def ingredients_calculation(persons):
+    cook_book = [['салат', [
+        ['картофель', 100, 'гр.'],
+        ['морковь', 50, 'гр.'],
+        ['огурцы', 50, 'гр.'],
+        ['горошек', 30, 'гр.'],
+        ['майонез', 70, 'мл.'], ]],
+                 ['пицца',
+                  [
+                      ['сыр', 50, 'гр.'],
+                      ['томаты', 50, 'гр.'],
+                      ['тесто', 100, 'гр.'],
+                      ['бекон', 30, 'гр.'],
+                      ['колбаса', 30, 'гр.'],
+                      ['грибы', 20, 'гр.'],
+                  ],
+                  ],
+                 ['фруктовый десерт',
+                  [
+                      ['хурма', 60, 'гр.'],
+                      ['киви', 60, 'гр.'],
+                      ['творог', 60, 'гр.'],
+                      ['сахар', 10, 'гр.'],
+                      ['мед', 50, 'мл.'],
+                  ]
+                  ]
+                 ]
+
+    for name in cook_book:
+        print(f'\n{cook_book[cook_book.index(name)][0].capitalize()}:')
+        for c in cook_book[cook_book.index(name)][1]:
+            print(f'{c[0]}, {c[1] * int(persons)}гр.')
+
+
+if __name__ == '__main__':
+    first_list = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+    second_list = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
+    acquaintance(first_list, second_list)
+    persons = int(input('Введите количество персон: '))
+    ingredients_calculation(persons)
