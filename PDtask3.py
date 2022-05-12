28d9a230078f10b53534f1a49bbd5c5064028bdd
dsamoob <dsamoob@yandex.ru>
2022-05-12 22:17:52 +0300

    no message

--- PDtask3.py
+++ PDtask3.py
@@ -1,11 +1,11 @@
 def acquaintance(first_list, second_list):
-    if len(first_list) != len(second_list):
-        print(f'Списки разного размера - не получится спарить')
-    else:
+    if len(first_list) == len(second_list):
         coupling = list(zip(sorted(first_list), sorted(second_list)))
         for i in coupling:
             print(f'{i[0]} и {i[1]}')
         print(f'\n')
+    else:
+        print(f'Списки разного размера - не получится спарить')
 
 
 def ingredients_calculation(persons):
@@ -36,15 +36,14 @@ def ingredients_calculation(persons):
                   ]
                  ]
 
-    for name in cook_book:
-        print(f'\n{cook_book[cook_book.index(name)][0].capitalize()}:')
-        for c in cook_book[cook_book.index(name)][1]:
-            print(f'{c[0]}, {c[1] * int(persons)}гр.')
+    for i, j in cook_book:
+        print('\n', i.capitalize(), sep='')
+        for name, volume, gr in j:
+            print(f'{name} {volume * persons} {gr}.')
 
 
 if __name__ == '__main__':
     first_list = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
     second_list = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
     acquaintance(first_list, second_list)
-    persons = int(input('Введите количество персон: '))
-    ingredients_calculation(persons)
+    ingredients_calculation(int(input('Введите количество персон: ')))
