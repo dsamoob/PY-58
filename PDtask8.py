9f367929e6a04d2776c49d5487de6fcdf3dcca88
dsamoob <dsamoob@yandex.ru>
2022-06-24 13:12:41 +0300

    finished task7. small updates.

---
+++ PDtask8.py
@@ -0,0 +1,37 @@
+import requests
+from pprint import pprint
+import json
+from YandexDisk import YandexDisk
+
+
+
+def heroes_checking(url, heroes_list:list):
+    response = requests.get(url).json()
+    best_hero = ''
+    best_brain = 0
+    for name in heroes_list:
+        for item in response:
+            if name == item['name'] and best_brain < item['powerstats']['intelligence']:
+                best_hero = item['name']
+                best_brain = item['powerstats']['intelligence']
+    return f'{best_hero} is the best becoz his "int" = {best_brain}'
+
+
+if __name__ == '__main__':
+    TOKEN =
+    heroes_url = "https://akabab.github.io/superhero-api/api/all.json"
+    heroes_list = ['Hulk', 'Captain America', 'Thanos']
+    print(heroes_checking(heroes_url, heroes_list))
+
+    ya = YandexDisk(token=TOKEN)
+    ya.upload_file_to_disk(disk_file_path="/123/text.xls",
+                           filename="/Users/egorbelov/GitHub/PY-58/dif_files/Recipe.txt")
+    headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {TOKEN}'
+        }
+    ya.make_new_directory('ubunthu')
+    pprint(ya.get_files_list())
+    ya.delete_file('Recipe3.txt')
+
+
---
+++ YandexDisk.py
@@ -0,0 +1,57 @@
+import requests
+
+class YandexDisk:
+    def __init__(self, token):
+        self.token = token
+
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files?limit=1000'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        response_href = self._get_upload_link(disk_file_path=disk_file_path)
+        print(response_href)
+        url = response_href.get("href", "")
+        response = requests.put(url, data=open(filename, 'rb'))
+        print(response)
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('success')
+
+    def make_new_directory(self, dirname: str):
+        url = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = self.get_headers()
+        response = requests.put(f'{url}?path={dirname}', headers=headers)
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('success')
+
+    def delete_file(self, file):
+        url = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = self.get_headers()
+        response = requests.delete(f'{url}?path={file}', headers=headers)
+        response.raise_for_status()
+        if response.status_code == 204:
+            print('success')
+
+
+
+
+
+
